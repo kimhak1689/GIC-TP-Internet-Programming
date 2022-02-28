b9c5fd4c91ff6d854c7a978017a001f6ab969f32
HAN KIMHAK <hankimhak@HANs-MacBook-Pro.local>
2022-02-28 19:42:35 +0700

    finish Exercise01

--- TP07/01/index.js
+++ TP07/01/index.js
@@ -11,7 +11,12 @@ app.get('/', function (req, res) {
 });
 
 app.get('/detail', function (req, res) {
-    res.send("detail page");
+    // res.send("detail page");
+    readFile('./book-detail.html', 'utf-8', (err, html)=>{
+        if(err)
+            res.status(500).send("Error");
+        res.send(html)
+    })
 });
 
 app.listen(3000, () => {
